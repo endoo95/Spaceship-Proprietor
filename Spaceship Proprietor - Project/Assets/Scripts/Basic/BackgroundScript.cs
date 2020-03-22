a3cebb804cd6b2f21bb58b7c7abb39eb8009b6a1
Endoo <endoo95@gmail.com>
2020-03-23 00:17:48 +0100

    UPDATE with working gitignore

---
+++ Spaceship Proprietor - Project/Assets/Scripts/Basic/BackgroundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    public float parralax = 4;
+
+    private void Update()
+    {
+        MeshRenderer mr;
+        mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.y / transform.localScale.y / parralax;
+
+        mat.mainTextureOffset = offset;
+    }
+}
