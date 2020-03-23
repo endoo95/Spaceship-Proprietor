66ad66118b041d4f0c61e9067bac46afba3b7a53
Endoo <endoo95@gmail.com>
2020-03-23 01:02:30 +0100

    UPDATE Project with working .gitignore
    Again :p

---
+++ Spaceship Proprietor - Project/Assets/Scripts/Basic/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform myTarget;
+    public Vector3 offset;
+
+    [SerializeField] private float followSpeed = 0.2f; //Best for from 0 to 1
+
+    void FixedUpdate()
+    {
+        if (myTarget != null)
+        {
+            offset.z = transform.position.z;
+            Vector3 targetPosition = myTarget.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, followSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
